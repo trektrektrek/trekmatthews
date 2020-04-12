82df36a959ba6d2a13619928c0802be307646fd0
Trek Matthews <trek@Treks-MacBook-Pro.local>
2020-04-12 15:55:32 -0500

    init files

---
+++ ndxzsite/config/config.php
@@ -0,0 +1,8 @@
+<?php  if (!defined('SITE')) exit('No direct script access allowed');
+
+$indx['db'] 		= 'trekmatt_indexhibit';
+$indx['user'] 		= 'trekmatt_index';
+$indx['pass'] 		= 'Breathein1';
+$indx['host'] 		= 'localhost';
+$indx['sql']		= 'mysql';
+define('PX', 'ndxz_');
